3c61bf4ad450c6da753381bde45fd66e97545953
Valentin Lab <valentin.lab@kalysto.org>
2023-04-12 11:39:58 +0200

    new: make ``block_rate`` command configurable on the command line

--- src/pyc3l_cli/cmd/block_rate.py
+++ src/pyc3l_cli/cmd/block_rate.py
@@ -1,49 +1,65 @@
 #!/usr/bin/env python
 """Monitors block rate"""
 
-
-from pyc3l.ApiCommunication import ApiCommunication
-from datetime import datetime, timedelta
+import click
 import time
 
-# load the high level functions
-api_com = ApiCommunication("Lemanopolis")
+from pyc3l.ApiCommunication import ApiCommunication
+from pyc3l_cli import common
 
 
-# configure run
-test_duration = 20  # [min]
-step = 2  # [sec]
+@click.command()
+@click.option("-d", "--duration",
+              help="stop monitoring after given minutes",
+              default=None,
+              type=int)
+@click.option("-D", "--delay", help="delay between blockchain request in seconds", default=2)
+@click.option("-e", "--endpoint",
+              help="Force com-chain endpoint.")
+@click.option("-c", "--currency",
+              help="Force com-chain endpoint.",
+              default="Lemanoplis")
+def run(duration, delay, endpoint, currency):
 
-number = int((test_duration * 60) / step)
 
-# test run
-blocks = []
-dt = []
-print("Starting the run. See you in " + str(test_duration) + " min")
-start_block = api_com.getBlockNumber()
-start_time = datetime.now()
-for counter in range(number):
-    curr_block = api_com.getBlockNumber()
-    curr_time = datetime.now()
-    if curr_block > start_block:
-        delta = curr_time - start_time
-        sec = delta.total_seconds()
-        blocks.append(start_block)
-        dt.append(sec)
-        start_block = curr_block
-        start_time = curr_time
-        print("New block after " + str(sec) + " s")
-    time.sleep(step)
+    # load the high level functions
+    api_com = ApiCommunication(currency, endpoint)
 
-# output result
-total_block = len(blocks)
+    start = time.time()
 
-print(
-    "During the "
-    + str(test_duration)
-    + " min run "
-    + str(total_block)
-    + " blocks where added average delay ="
-    + str(sum(dt) / total_block)
-    + " s."
-)
+    # test run
+    blocks = []
+    deltas = []
+    msg = (f"Run will stop in {duration} min"
+           if duration else
+           "Cancel run with Ctrl-C.")
+    print(f"Starting the run. {msg}")
+    current_block = api_com.getBlockNumber()
+    current_block_time = time.time()
+    if duration:
+        duration = duration * 60
+    try:
+        while True:
+            new_block = api_com.getBlockNumber()
+            current_time = time.time()
+            if new_block > current_block:
+                delta = current_time - current_block_time
+                current_block_time = current_time
+                blocks.append(current_block)
+                deltas.append(delta)
+                current_block = new_block
+                print(f"New block after {common.pp_duration(delta)}")
+            if duration and (current_time - start) > duration:
+                break
+            time.sleep(delay)
+    except KeyboardInterrupt:
+        pass
+    msg = f"During the {common.pp_duration(current_time - start)} run, "
+    if blocks:
+        msg += (
+            f"{len(blocks)} blocks where added. " +
+            f"Average delay for new block is {common.pp_duration(sum(deltas) / len(blocks))}"
+        )
+    else:
+        msg += "No blocks where added !"
+    print(msg)
