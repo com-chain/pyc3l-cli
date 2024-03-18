6f35cccfda284bb900d2695e35898071f9fe8791
Valentin Lab <valentin.lab@kalysto.org>
2024-03-18 17:51:48 +0100

    new: add new ``pledge`` script

---
+++ src/pyc3l_cli/cmd/pledge.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+
+import click
+import getpass
+
+from pyc3l_cli import common
+from pyc3l import Pyc3l
+
+
+@click.command()
+@click.option("-w", "--wallet-file", help="wallet path")
+@click.option("-p", "--password-file", help="wallet password path")
+@click.option("-e", "--endpoint", help="Force com-chain endpoint.")
+@click.argument("address", type=str, required=True)
+@click.argument("amount", type=float, required=True)
+def run(wallet_file, password_file, endpoint, address, amount):
+    pyc3l = Pyc3l(endpoint)
+
+    wallet = pyc3l.Wallet.from_file(
+        wallet_file or common.filepicker("Select Admin Wallet")
+    )
+
+    wallet.unlock(
+        common.load_password(password_file) if password_file else getpass.getpass()
+    )
+
+    currency = wallet.currency
+
+    account = currency.Account(address)
+    status = account.Status
+    print("Account " + address + " is currently active = " + str(status))
+    print("Balance = " + str(account.GlobalBalance))
+
+    res = wallet.pledge(address, amount)
+    print(res)
+    print("")
+
+
+if __name__ == "__main__":
+    run()
