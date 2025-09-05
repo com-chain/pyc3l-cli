b3f0c51935b32a6b3d51b764fa5246584e0a15e8
Valentin Lab <valentin.lab@kalysto.org>
2025-09-05 14:58:40 +0200

    new: [report] move main store code to ``pyc3l``

--- src/pyc3l_cli/common.py
+++ src/pyc3l_cli/common.py
@@ -585,19 +585,3 @@ def protect_tty(f):
             enable_echo(old_tty_settings)
             show_cursor()
     return wrapper
-
-
-def init_cache_dirs():
-    """Create the cache directory if it doesn't exist and returns it's path."""
-
-    if os.environ.get("PYC3L_CACHE_DIR"):
-        PYC3L_CACHE_DIR = os.environ.get("PYC3L_CACHE_DIR")
-    else:
-        if os.geteuid() == 0:
-            PYC3L_CACHE_DIR = "/var/cache/pyc3l"
-        else:
-            PYC3L_CACHE_DIR = os.path.expanduser("~/.cache/pyc3l")
-
-    if not os.path.exists(PYC3L_CACHE_DIR):
-        os.makedirs(PYC3L_CACHE_DIR)
-    return PYC3L_CACHE_DIR
